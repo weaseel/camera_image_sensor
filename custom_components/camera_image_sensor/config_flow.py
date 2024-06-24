408eb7c77f147996a323a1a2184dbeaf2142d07d
weaseel <dasweasel99@gmail.com>
2024-06-24 11:46:54 +0200

    Debug logging

--- custom_components/camera_image_sensor/config_flow.py
+++ custom_components/camera_image_sensor/config_flow.py
@@ -3,15 +3,20 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers.selector import EntitySelector, EntitySelectorConfig
 import homeassistant.helpers.config_validation as cv
+import logging
 
 from . import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 class CameraImageSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        _LOGGER.debug("Starting async_step_user with user_input: %s", user_input)
         errors = {}
         if user_input is not None:
+            _LOGGER.debug("Creating entry with user_input: %s", user_input)
             return self.async_create_entry(title="Camera Image Sensor", data=user_input)
 
         return self.async_show_form(
@@ -26,6 +31,7 @@ class CameraImageSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
+        _LOGGER.debug("Getting options flow for config_entry: %s", config_entry)
         return CameraImageSensorOptionsFlowHandler(config_entry)
 
 class CameraImageSensorOptionsFlowHandler(config_entries.OptionsFlow):
@@ -33,7 +39,9 @@ class CameraImageSensorOptionsFlowHandler(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
+        _LOGGER.debug("Starting async_step_init with user_input: %s", user_input)
         if user_input is not None:
+            _LOGGER.debug("Creating entry with user_input: %s", user_input)
             return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
