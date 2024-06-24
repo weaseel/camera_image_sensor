295e4bb3984be9f289a4472ba0883445f1c51cd7
weaseel <dasweasel99@gmail.com>
2024-06-24 10:08:05 +0200

    Modified structure

---
+++ custom_components/camera_image_sensor/config_flow.py
@@ -0,0 +1,38 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.helpers.selector import EntitySelector, EntitySelectorConfig
+
+from . import DOMAIN
+
+class CameraImageSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Camera Image Sensor", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                "camera_entity": EntitySelector(EntitySelectorConfig(domain="camera")),
+            })
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return CameraImageSensorOptionsFlowHandler(config_entry)
+
+class CameraImageSensorOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional("scan_interval", default=self.config_entry.options.get("scan_interval", "00:01:00")): cv.time_period
+            })
+        )
