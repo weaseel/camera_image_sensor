74978f03ba1b64665334f6a77aed636f38aa35d4
weaseel <dasweasel99@gmail.com>
2024-06-24 11:36:16 +0200

    Other fixes

--- custom_components/camera_image_sensor/config_flow.py
+++ custom_components/camera_image_sensor/config_flow.py
@@ -2,11 +2,13 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers.selector import EntitySelector, EntitySelectorConfig
-import homeassistant.helpers.config_validation as cv  # Make sure this import is here
+import homeassistant.helpers.config_validation as cv
 
 from . import DOMAIN
 
 class CameraImageSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
@@ -15,8 +17,8 @@ class CameraImageSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                "camera_entity": EntitySelector(EntitySelectorConfig(domain="camera")),
-                vol.Optional("scan_interval", default="00:01:00"): cv.time_period
+                vol.Required("camera_entity"): EntitySelector(EntitySelectorConfig(domain="camera")),
+                vol.Optional("scan_interval", default="00:01:00"): cv.time_period_str
             }),
             errors=errors
         )
@@ -37,6 +39,6 @@ class CameraImageSensorOptionsFlowHandler(config_entries.OptionsFlow):
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Optional("scan_interval", default=self.config_entry.options.get("scan_interval", "00:01:00")): cv.time_period
+                vol.Optional("scan_interval", default=self.config_entry.options.get("scan_interval", "00:01:00")): cv.time_period_str
             })
         )
