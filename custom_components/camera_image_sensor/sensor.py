00491fe50a2c4cf540efdcf0b471035b4c2be974
weaseel <dasweasel99@gmail.com>
2024-06-24 11:05:20 +0200

    fixed

--- custom_components/camera_image_sensor/sensor.py
+++ custom_components/camera_image_sensor/sensor.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
